e9c05f29c26de47c9ccabe794acdb1cfc372ed5d
harryghgim <harryghgim@gmail.com>
2021-04-07 19:57:12 +0900

    카카오 맵 api 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// 참고: https://velog.io/@bearsjelly/React-kakao-%EC%A7%80%EB%8F%84-%EB%9D%84%EC%9A%B0%EA%B8%B0-2-%EC%95%B1%ED%82%A4%EB%A5%BC-%EC%9D%B4%EC%9A%A9%ED%95%B4-%EC%A7%80%EB%8F%84-%EB%9D%84%EC%9A%B0%EA%B8%B0
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*global kakao*/ 
+
+class App extends React.Component {
+  componentDidMount() {
+    const script = document.createElement("script");
+    const appkey = process.env.REACT_APP_APPKEY
+    script.async = true;
+    
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${appkey}&autoload=false&libraries=services`;
+    document.head.appendChild(script)
+
+    script.onload = () => {
+      kakao.maps.load( () => {
+        let container = document.getElementById("map")
+        let options = {
+          center: new kakao.maps.LatLng(37.506502, 127.053617),
+          level: 8
+        }
+
+        const map = new kakao.maps.Map(container, options)
+      })
+    }
+  }
+
+  render() {
+      return (
+          <div id="map" style={{width:500, height:300}}/>
+      )
+  }
 }
 
 export default App;
